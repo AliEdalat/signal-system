fb0b1b01d5085260c64e557dee684d5124939342
AliEdalat <ali.edalat.77@gmail.com>
2018-11-24 18:16:35 +0330

    p3 updated

--- p3.py
+++ p3.py
@@ -4,6 +4,27 @@ from matplotlib import pyplot as plt
 def u(n):
 	return (n >= 0)
 
+def d(n):
+	return (n==0)
+
+def x1():
+	n = np.array([n for n in range(-5, 10)])
+	x = d(n)
+	x2 = 2*d(n)
+	y = np.sin((np.pi/2)*x)
+	y2 = np.sin((np.pi/2)*x2)
+	plt.stem(x, y)
+	plt.title('Y[n]')
+	plt.xlabel('n')
+	plt.ylabel('Y[n]')
+	plt.show()
+	plt.stem(x2, y2)
+	plt.title('Y2[n]')
+	plt.xlabel('n')
+	plt.ylabel('Y2[n]')
+	plt.show()
+	
+
 def x2():
 	n = np.array([n for n in range(-5, 10)])
 	plt.stem(n, u(n))
